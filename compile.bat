d3a2ad61f3517d0be69718f9239c9bccde59fe50
Neko Gong <g405252865@163.com>
2018-02-03 13:51:56 +0800

    modify compile.bat, always use the newest compiler

--- compile.bat
+++ compile.bat
@@ -2,7 +2,12 @@
 
 if "%1"=="clean" goto clean
 
-set compile-tool="C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe"
+:: we always use the newest compiler to compile the project
+set compile-tool=
+for /f %%i in ('dir C:\Windows\Microsoft.NET\Framework\csc.exe /b /s') do (
+    set compile-tool=%%i
+)
+if "%compile-tool%"=="" goto end
 %compile-tool% /target:winexe /out:encrypt-form.exe /win32icon:encrypt-icon.ico DecryptTool\Utils.cs DecryptTool\EncryptTool.cs DecryptTool\DecryptTool.cs DecryptTool\CryptologyForm.cs DecryptTool\StartEncryptForm.cs
 %compile-tool% /target:winexe /out:decrypt-form.exe /win32icon:encrypt-icon.ico DecryptTool\Utils.cs DecryptTool\EncryptTool.cs DecryptTool\DecryptTool.cs DecryptTool\CryptologyForm.cs DecryptTool\StartDecryptForm.cs
 %compile-tool%  /out:kanban-form.exe /win32icon:board-icon.ico Kanban\KanbanMainForm.cs
