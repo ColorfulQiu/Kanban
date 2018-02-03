e997cfea14546ba83fa1d6456035fe86e2ecd1b1
Neko Gong <g405252865@163.com>
2018-02-03 13:33:06 +0800

    add the feature of dragging boards

--- Kanban/KanbanMainForm.cs
+++ Kanban/KanbanMainForm.cs
@@ -5,8 +5,13 @@ using System.Collections.Generic;
 
 namespace kanban.main.form
 {
-   class KanbanMainForm : Form
-   {
+    public class PanelTag {
+        public int taskId { set; get; }
+        public int column { set; get; }     // 0: to do, 1: in progress, 2: done
+        public int row {set; get; }
+    }
+
+    public class KanbanMainForm : Form {
 		protected Label _labelUserInfo;
         protected CheckBox _checkBoxShowMineOnly;
         protected Button _buttonShowOverdueTask;
@@ -25,6 +30,10 @@ namespace kanban.main.form
         private static readonly Point TODO_FIRST_POSITION = new Point(LEFT_MARGIN, 200);
         private static readonly Point INPROGRESS_FIRST_POSITION = new Point(BOARD_WIDTH + LEFT_MARGIN + WIDTH_INTERVAL, 200);
         private static readonly Point DONE_FIRST_POSITION = new Point(BOARD_WIDTH * 2 + LEFT_MARGIN + WIDTH_INTERVAL * 2, 200);
+
+        private bool boardClicked = false;
+        private Point boardOriginPosition = new Point();
+        private Point mouseOriginPosition = new Point();
 
         public KanbanMainForm()
         {
@@ -119,6 +128,16 @@ namespace kanban.main.form
             board.Controls.Add(taskLabel);
             board.Controls.Add(nameLabel);
 
+            board.MouseDown += new MouseEventHandler(boardMouseDown);
+            board.MouseMove += new MouseEventHandler(boardMouseMove);
+            board.MouseUp += new MouseEventHandler(boardMouseUp);
+            taskLabel.MouseDown += new MouseEventHandler(boardMouseDown);
+            taskLabel.MouseMove += new MouseEventHandler(boardMouseMove);
+            taskLabel.MouseUp += new MouseEventHandler(boardMouseUp);
+            nameLabel.MouseDown += new MouseEventHandler(boardMouseDown);
+            nameLabel.MouseMove += new MouseEventHandler(boardMouseMove);
+            nameLabel.MouseUp += new MouseEventHandler(boardMouseUp);
+
             return board;
         }
 
@@ -143,6 +162,7 @@ namespace kanban.main.form
 
         public void generateToDoBoard(string task, char name) {
             Panel panelBoard = generateCommonBoard(task, name);
+            panelBoard.Tag = new PanelTag () { column = 0, row = _panelToDoList.Count };
             panelBoard.BackColor = Color.Green;
             _panelToDoList.Add(panelBoard);
             refresh();
@@ -150,6 +170,7 @@ namespace kanban.main.form
 
         public void generateInProgressBoard(string task, char name) {
             Panel panelBoard = generateCommonBoard(task, name);
+            panelBoard.Tag = new PanelTag () { column = 1, row = _panelInProgessList.Count };
             panelBoard.BackColor = Color.Blue;
             _panelInProgessList.Add(panelBoard);
             refresh();
@@ -157,13 +178,91 @@ namespace kanban.main.form
 
         public void generateDoneBoard(string task, char name) {
             Panel panelBoard = generateCommonBoard(task, name);
+            panelBoard.Tag = new PanelTag () { column = 2, row = _panelDoneList.Count };
             panelBoard.BackColor = Color.Red;
             _panelDoneList.Add(panelBoard);
             refresh();
         }
 
+        // Listeners
+
         private void showOverdueTask(object sender, EventArgs args) {
             this.generateToDoBoard("1This is my first task, and I should finish the C# kanban board", 'N');
+        }
+
+        private void boardMouseDown(object sender, MouseEventArgs args) {
+            Panel board = sender is Panel? (Panel) sender: (Panel) (((Control) sender).Parent);
+            board.BringToFront();
+            boardClicked = true;
+            boardOriginPosition = board.Location;
+            mouseOriginPosition = new Point(args.X + board.Location.X, args.Y + board.Location.Y);
+        }
+
+        private void boardMouseMove(object sender, MouseEventArgs args) {
+            Panel board = sender is Panel? (Panel) sender: (Panel) (((Control) sender).Parent);
+            if(boardClicked) {
+                Point mousePostion = new Point(args.X + board.Location.X, args.Y + board.Location.Y);
+                board.Location = new Point(mousePostion.X - mouseOriginPosition.X + boardOriginPosition.X, 
+                                           mousePostion.Y - mouseOriginPosition.Y + boardOriginPosition.Y);
+            }
+        }
+
+        private void boardMouseUp(object sender, MouseEventArgs args) {
+            Panel board = sender is Panel? (Panel) sender: (Panel) (((Control) sender).Parent);
+            boardClicked = false;
+            boardOriginPosition = new Point();
+            mouseOriginPosition = new Point();
+
+            Point boardCenter = new Point(board.Location.X + BOARD_WIDTH / 2, board.Location.Y + BOARD_HEIGHT / 2);
+            Console.WriteLine("center: " + boardCenter);
+            int targetStatus = -1;
+            if(boardCenter.X < INPROGRESS_FIRST_POSITION.X) {
+                targetStatus = 0;
+            } else if(boardCenter.X < DONE_FIRST_POSITION.X) {
+                targetStatus = 1;
+            } else {
+                targetStatus = 2;
+            }
+
+            movingBoardToStatus(board, targetStatus);
+        }
+
+        private void movingBoardToStatus(Panel board, int targetStatus) {
+            PanelTag tag = (PanelTag) (board.Tag);
+            Console.WriteLine(tag.column + ", " + tag.row);
+            Console.WriteLine("targetStatus = " + targetStatus);
+            if(tag.column != targetStatus) {
+                if(tag.column == 0) {
+                    _panelToDoList.RemoveAt(tag.row);
+                } else if(tag.column == 1) {
+                    _panelInProgessList.RemoveAt(tag.row);
+                } else if(tag.column == 2) {
+                    _panelDoneList.RemoveAt(tag.row);
+                }
+
+                if(targetStatus == 0) {
+                    board.Tag = new PanelTag() { column = targetStatus, row = _panelToDoList.Count };
+                    _panelToDoList.Add(board);
+                } else if(targetStatus == 1) {
+                    board.Tag = new PanelTag() { column = targetStatus, row = _panelInProgessList.Count };
+                    _panelInProgessList.Add(board);
+                } else if(targetStatus == 2) {
+                    board.Tag = new PanelTag() { column = targetStatus, row = _panelDoneList.Count };
+                    _panelDoneList.Add(board);
+                }
+            }
+
+            for(int i = 0; i < _panelToDoList.Count; i++) {
+                _panelToDoList[i].Tag = new PanelTag() { column = 0, row = i };
+            }
+            for(int i = 0; i < _panelInProgessList.Count; i++) {
+                _panelInProgessList[i].Tag = new PanelTag() { column = 1, row = i };
+            }
+            for(int i = 0; i < _panelDoneList.Count; i++) {
+                _panelDoneList[i].Tag = new PanelTag() { column = 2, row = i };
+            }
+
+            refresh();
         }
 
         public static void Main() {
